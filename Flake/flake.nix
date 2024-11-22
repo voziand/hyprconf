d6f90344cddf389698373e115ff2fcca6b9db224
vozian <60975283+voziand@users.noreply.github.com>
2024-11-21 23:09:44 -0800

    Update flake.nix

--- Flake/flake.nix
+++ Flake/flake.nix
@@ -11,6 +11,14 @@
 
     let
       system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+        # use this variant if unfree packages are needed:
+        # unstable = import nixpkgs-unstable {
+        #   inherit system;
+        #   config.allowUnfree = true;
+        # };
+      };
     in {
 
     # nixos - system hostname
@@ -23,6 +31,7 @@
         inherit inputs system;
       };
       modules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
         ./configuration.nix
       ];
     };
