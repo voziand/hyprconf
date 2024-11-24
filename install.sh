061d900b33ece23ac4273d096e661cf74beb700a
vozian <60975283+voziand@users.noreply.github.com>
2024-11-23 23:45:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 systemctl --user enable pipewire.service
 systemctl --user enable wireplumber.service
+
+git clone https://github.com/alvatip/Nordzy-icon
+cd Nordzy-icon/
+sudo ./install.sh -t green -c dark
+cd ..
