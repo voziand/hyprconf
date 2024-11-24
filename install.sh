0f28155edd9624838fd9918b7866082cd1be1a51
vozian <60975283+voziand@users.noreply.github.com>
2024-11-23 23:57:28 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ git clone https://github.com/alvatip/Nordzy-icon
 cd Nordzy-icon/
 sudo ./install.sh -t green -c dark
 cd ..
+
+git clone https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme.git
+cd themes
+./install.sh -c dark -t green -s compact -l
+cd ..
+cd ..
