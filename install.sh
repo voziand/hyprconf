da1842122a736cda649fd60d6c2280655d27b996
vozian <60975283+voziand@users.noreply.github.com>
2025-02-11 09:25:14 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,6 @@
 currentdir=$(pwd)
-#user=$(whoami)
-
+# install packages
 xargs -a packages.txt sudo zypper install -y
-
-git clone https://github.com/alvatip/Nordzy-icon
-cd Nordzy-icon/
-sudo ./install.sh -t green -c dark
-cd "$currentdir"
-
-git clone https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme.git
-cd Everforest-GTK-Theme/themes
-./install.sh -c dark -t green -s compact -l
-cd "$currentdir"
-
-if [ -d "$HOME/.config" ] 
-then
-    cp -r dotfiles/* "$HOME/.config/"
-else
-    mkdir "$HOME/.config"
-    cp -r dotfiles/* "$HOME/.config/"
-fi
-
-# set configuration scripts to be executable
-chmod +x $HOME/.config/hypr/start.sh
-for x in `ls $HOME/.config/waybar/scripts` ; do chmod +x $x; done
-
 sudo zypper addrepo https://download.opensuse.org/repositories/home:mohms/openSUSE_Tumbleweed/home:mohms.repo -y
 sudo zypper refresh -y
 sudo zypper install -y nerd-fonts-firacode
@@ -39,6 +15,29 @@ sudo mv ./bicep /usr/local/bin/bicep
 # bicep language server
 curl -fLO https://github.com/Azure/bicep/releases/latest/download/bicep-langserver.zip
 sudo unzip -d /usr/local/bin/bicep-langserver bicep-langserver.zip
+
+# looks
+# icon theme
+git clone https://github.com/alvatip/Nordzy-icon
+cd Nordzy-icon/
+sudo ./install.sh -t green -c dark
+cd "$currentdir"
+# gtk theme
+git clone https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme.git
+cd Everforest-GTK-Theme/themes
+./install.sh -c dark -t green -s compact -l
+cd "$currentdir"
+# dotfiles
+if [ -d "$HOME/.config" ] 
+then
+    cp -r dotfiles/* "$HOME/.config/"
+else
+    mkdir "$HOME/.config"
+    cp -r dotfiles/* "$HOME/.config/"
+fi
+# set configuration scripts to be executable
+chmod +x $HOME/.config/hypr/start.sh
+for x in `ls $HOME/.config/waybar/scripts` ; do chmod +x $x; done
 # wallpaper
 mkdir -p ~/Pictures/wallpapers
 cp  everforest.jpg ~/Pictures/wallpapers/
